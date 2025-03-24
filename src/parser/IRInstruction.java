cd13a7e82d9e80173fd493b899a4dacc8ccc7f89
Kevin Luna <L21020397@veracruz.tecnm.mx>
2025-03-23 21:18:52 -0600

    Se agregaron instrucciones nuevas al codigo intermedio.

--- src/parser/IRInstruction.java
+++ src/parser/IRInstruction.java
@@ -1,5 +1,5 @@
 package parser;
 
 public enum IRInstruction {
-    ADD, SUB, MUL, DIV, MOD, AND, OR, XOR, NOT, IF, ELSE , FOR, WHILE, DI, DF, DC, DS, DB, PI, PF, PC, PB, PS, LBL;
+    ADD, SUB, MUL, DIV, MOD, AND, OR, XOR, NOT, EQ, NEQ, LT, GT, LEQ, GEQ, IF , DI, DR, DC, DS, DB, PI, PR, PC, PB, PS, RI, RR, RC, RB, RS, LBL, JMP, ENDL, PGRM;
 }
