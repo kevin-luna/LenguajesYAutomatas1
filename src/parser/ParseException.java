07b20cf9a6409abc2d557a1de6311babbbb5cc61
Kevin Luna <L21020397@veracruz.tecnm.mx>
2025-03-24 12:31:37 -0600

    Se tradujeron los mensajes de error al español para las clases ParseException y TokenMgrError.

--- src/parser/ParseException.java
+++ src/parser/ParseException.java
@@ -108,7 +108,7 @@ public class ParseException extends Exception {
       }
       expected.append(EOL).append("    ");
     }
-    String retval = "Encountered \"";
+    String retval = "Se encontro \"";
     Token tok = currentToken.next;
     for (int i = 0; i < maxSize; i++) {
       if (i != 0) retval += " ";
@@ -123,7 +123,7 @@ public class ParseException extends Exception {
       tok = tok.next;
     }
     if (currentToken.next != null) {
-      retval += "\" at line " + currentToken.next.beginLine + ", column " + currentToken.next.beginColumn;
+      retval += "\" en la linea " + currentToken.next.beginLine + ", columna " + currentToken.next.beginColumn;
     }
     retval += "." + EOL;
     
@@ -132,9 +132,9 @@ public class ParseException extends Exception {
         // Nothing to add here
     } else {
 	    if (expectedTokenSequences.length == 1) {
-	      retval += "Was expecting:" + EOL + "    ";
+	      retval += "Se esperaba:" + EOL + "    ";
 	    } else {
-	      retval += "Was expecting one of:" + EOL + "    ";
+	      retval += "Se esperaba un:" + EOL + "    ";
 	    }
 	    retval += expected.toString();
     }
--- src/parser/TokenMgrError.java
+++ src/parser/TokenMgrError.java
@@ -103,12 +103,12 @@ public class TokenMgrError extends Error
    * Note: You can customize the lexical error message by modifying this method.
    */
   protected static String LexicalErr(boolean EOFSeen, int lexState, int errorLine, int errorColumn, String errorAfter, int curChar) {
-    return("Lexical error at line " + //
-          errorLine + ", column " + //
-          errorColumn + ".  Encountered: " + //
+    return("Error lexico en la linea " + //
+          errorLine + ", columna " + //
+          errorColumn + ".  Se encontro: " + //
           (EOFSeen ? "<EOF>" : ("'" + addEscapes(String.valueOf(curChar)) + "' (" + curChar + "),")) + //
-          (errorAfter == null || errorAfter.length() == 0 ? "" : " after prefix \"" + addEscapes(errorAfter) + "\"")) + //
-          (lexState == 0 ? "" : " (in lexical state " + lexState + ")");
+          (errorAfter == null || errorAfter.length() == 0 ? "" : " despues del prefijo \"" + addEscapes(errorAfter) + "\"")) + //
+          (lexState == 0 ? "" : " (en el estado lexico " + lexState + ")");
   }
 
   /**
