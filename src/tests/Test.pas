dff8743b1f3a4e25c05cf31d444e7eed3359c4e8
kevin-luna <L21020397@veracruz.tecnm.mx>
2025-05-27 22:50:25 -0600

    Se modifico el primer test por un clasico Hola Mundo.

--- src/tests/Test.pas
+++ src/tests/Test.pas
@@ -1,16 +1,6 @@
-program Prueba;
-var
-   a,b: integer;
+program Hola;
 const
-     PI: real = 3.141592;
-     SALUDO: string = 'HOLA MUNDO';
+     SALUDO: string = 'HOLA MUNDO!';
 begin
-  write();
-  write();
-  write();
-  write();
-  write();
-  readln(a,b);
-  if a>b then writeln(SALUDO);
-  else writeln ('a es igual que b');
+    writeln(SALUDO);
 end.
